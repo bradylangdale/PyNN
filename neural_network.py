4f804b55621087ecb4f17d86835d4da6f2ce1be4
Brady Langdale <bradylangdale@gmail.com>
2023-12-16 15:23:54 -0600

    Add frontend and ReLU

--- neural_network.py
+++ neural_network.py
@@ -24,13 +24,26 @@ class NeuralNetwork:
         self.layers[0] = input
 
         for i in range(1, len(self.layers)):
-            self.layers[i] = self.sigmoid((self.weights[i - 1] * self.layers[i - 1]) + self.bias[i - 1])
+            if i == len(self.layers) - 1:
+                self.layers[i] = self.sigmoid((self.weights[i - 1] * self.layers[i - 1]) + self.bias[i - 1])
+            else:
+                self.layers[i] = self.relu((self.weights[i - 1] * self.layers[i - 1]) + self.bias[i - 1])
 
     def sigmoid(self, m):
-        for i in range(m.rows):
-            m[i][0] = 1.0 / (1.0 + math.exp(-m[i][0]))
+        result = Matrix(m.rows, m.cols, rand=False)
 
-        return m
+        for i in range(m.rows):
+            result[i][0] = 1.0 / (1.0 + math.exp(-m[i][0]))
+
+        return result
+    
+    def relu(self, m):
+        result = Matrix(m.rows, m.cols, rand=False)
+
+        for i in range(m.rows):
+            result[i][0] = m[i][0] if m[i][0] > 0 else 0.01 * m[i][0]
+
+        return result
     
     def backward(self, output, rate_weights=0.5, rate_bias=0.5):
         dsig = self.dsigmoid(self.layers[-1])
@@ -46,10 +59,13 @@ class NeuralNetwork:
         self.bias[-1] -= rate_bias * delta
 
         for i in range(2, len(self.layers)):
-            dsig = self.dsigmoid(self.layers[-i])
-            dc = 2 * (self.layers[-(i - 1)].transpose() * delta)
+            dsig = self.drelu(self.layers[-i])
+            dc = 2 * (self.weights[-(i - 1)].transpose() * delta)
 
-            delta = dsig * dc
+            delta = Matrix(dsig.rows, dsig.cols, rand=False)
+            for j in range(dsig.rows):
+                delta[j][0] = dsig[j][0] * dc[j][0]
+
             dcost = delta * self.layers[-(i + 1)].transpose()
 
             self.weights[-i] -= rate_weights * dcost
@@ -64,6 +80,14 @@ class NeuralNetwork:
             result[i][0] = m[i][0] * (1 - m[i][0])
 
         return result
+    
+    def drelu(self, m):
+        result = Matrix(m.rows, m.cols, rand=False)
+
+        for i in range(m.rows):
+            result[i][0] = 1.0 if m[i][0] > 0 else 0.1
+
+        return result
 
     def size(self):
         length = []
@@ -74,54 +98,30 @@ class NeuralNetwork:
 
 if __name__ == '__main__':
     import random
-    import mnist_loader
-    import numpy as np
 
-
-    nn = NeuralNetwork([784, 32, 10])
-    data = list(list(mnist_loader.load_data_wrapper())[0])
-
-    j = 10
-    for i in range(len(data)):
-        train_set = list(data[i])
-        input = Matrix(784, 1, rand=False)
-        input.data = train_set[0]
-
-        output = Matrix(10, 1, rand=False)
-        output.data = train_set[1]
-
-        nn.forward(input)
-        cost = nn.backward(output)
-
-        if j == 10:
-            #print('Output', nn.layers[-1][0][0], 'Truth:', output[0][0])
-            print('Delta:', cost, 
-                  'Guess:', np.argmax(nn.layers[-1].data), 
-                  'Answer:', np.argmax(train_set[1]))
-            
-            print(nn.layers[-1])
-            print(train_set[1])
-            j = 0
-
-        j += 1
 
     # adding function example
-    '''nn = NeuralNetwork([2, 4, 4, 4, 1])   
+    nn = NeuralNetwork([2, 4, 4, 4, 1])   
 
     j = 1000
-    for i in range(1000000):
+    for i in range(200000):
         input = Matrix(2, 1, rand=False)
         input[0][0] = random.uniform(0.0, 0.5)
         input[1][0] = random.uniform(0.0, 0.5)
+        
         output = Matrix(1, 1, rand=False)
         output[0][0] = input[0][0] + input[1][0]
 
         nn.forward(input)
-        cost = nn.backward(output)
+
+        rate = output[0][0] - nn.layers[-1][0][0]
+        rate = max(0.1, min(rate, 0.99)) * 0.1
+
+        cost = nn.backward(output, rate, rate)
 
         if j == 1000:
             print('Output', nn.layers[-1][0][0], 'Truth:', output[0][0])
             print('Delta:', abs(nn.layers[-1][0][0] - output[0][0]))
             j = 0
 
-        j += 1'''
+        j += 1
