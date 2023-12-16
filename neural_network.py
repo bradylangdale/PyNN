42ff15d592e2e440176d4e01f2d4c13083357cbf
Brady Langdale <bradylangdale@gmail.com>
2023-12-15 19:31:45 -0600

    Bad backprop

--- neural_network.py
+++ neural_network.py
@@ -32,7 +32,7 @@ class NeuralNetwork:
 
         return m
     
-    def backward(self, output, rate_weights=0.01, rate_bias=0.001):
+    def backward(self, output, rate_weights=0.001, rate_bias=0.001):
         dsig = self.dsigmoid(self.layers[-1])
         dc = 2 * (self.layers[-1] - output)
 
@@ -47,15 +47,18 @@ class NeuralNetwork:
 
         for i in range(2, len(self.layers)):
             dsig = self.dsigmoid(self.layers[-i])
-            dc = 2 * (self.layers[-(i - 1)].transpose() * delta)
+            dc = 2 * (self.layers[-i] - delta)
 
-            delta = dsig * dc
+            delta = Matrix(dsig.rows, dsig.cols, rand=False)
+            for j in range(dsig.rows):
+                delta[j][0] = dsig[j][0] * dc[j][0]
+
             dcost = delta * self.layers[-(i + 1)].transpose()
 
             self.weights[-i] -= rate_weights * dcost
             self.bias[-i] -= rate_bias * delta
 
-        return sum([dc[i][0] for i in range(dc.rows)])
+        return sum(sum(x) for x in dc.data)
 
     def dsigmoid(self, m):
         result = Matrix(m.rows, m.cols, rand=False)
@@ -74,14 +77,45 @@ class NeuralNetwork:
 
 if __name__ == '__main__':
     import random
+    import mnist_loader
+    import numpy as np
 
-    nn = NeuralNetwork([2, 4, 1])
+
+    '''nn = NeuralNetwork([784, 32, 10])
+    data = list(list(mnist_loader.load_data_wrapper())[0])
+
+    j = 10
+    for i in range(len(data)):
+        train_set = list(data[i])
+        input = Matrix(784, 1, rand=False)
+        input.data = train_set[0]
+
+        output = Matrix(10, 1, rand=False)
+        output.data = train_set[1]
+
+        nn.forward(input)
+        cost = nn.backward(output)
+
+        if j == 10:
+            #print('Output', nn.layers[-1][0][0], 'Truth:', output[0][0])
+            print('Delta:', cost, 
+                  'Guess:', np.argmax(nn.layers[-1].data), 
+                  'Answer:', np.argmax(train_set[1]))
+            
+            print(nn.layers[-1])
+            print(train_set[1])
+            j = 0
+
+        j += 1'''
+
+    # adding function example
+    nn = NeuralNetwork([2, 2, 1])   
 
     j = 1000
     for i in range(1000000):
         input = Matrix(2, 1, rand=False)
-        input[0][0] = random.uniform(0, 0.5)
-        input[1][0] = random.uniform(0, 0.5)
+        input[0][0] = random.uniform(0.0, 0.35)
+        input[1][0] = random.uniform(0.0, 0.35)
         output = Matrix(1, 1, rand=False)
         output[0][0] = input[0][0] + input[1][0]
 
